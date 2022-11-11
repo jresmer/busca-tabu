16cd064f9f5213217353957e57d6f922281f9d96
jresmer <jpresmer@gmail.com>
2022-11-11 13:59:10 -0300

    initial commit

---
+++ code/tabuList.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+class TabuList:
+
+    # CRIA LISTA DE SOLUCOES
+    # SETTA NUMERO DE ITERACOES QUE UMA SOLUCAO DEVE PERMANECER NA LISTA
+    def __init__(self, iteration_counter=50, *args):
+        self.__iteration_counter = iteration_counter
+        self.__iteration_reset_value = iteration_counter
+        self.__list = list(args)
+
+    # MAGIC METHOD CHECAGEM IN
+    def __contains__(self, item):
+        return item in self.__list
+
+    # ADICIONA E REMOVE SOLUCOES TABU
+    def update(self, item=None):
+        if item is not None:
+            self.__list.append(item)
+        self.__iteration_counter -= 1
+        if self.__iteration_counter < 1:
+            self.__list.pop(0)
+            self.__iteration_counter = self.__iteration_reset_value
