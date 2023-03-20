a16e57c21b1fc08188bff3c0453bdffcd4912f75
jresmer <jpresmer@gmail.com>
2023-03-20 14:39:49 -0300

    updated to singletonMeta

--- code/logManager.py
+++ code/logManager.py
@@ -1,16 +1,7 @@
 from time import time
 
 
-class SingletonMeta(type):
-
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
+from singletonMeta import SingletonMeta
 
 
 class LogManager(metaclass=SingletonMeta):
