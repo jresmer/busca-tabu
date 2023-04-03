4d4ae64b25cf7c4616ef50b8c25ad25c7bc9df4c
jresmer <jpresmer@gmail.com>
2023-04-03 12:34:04 -0300

    operations made to generate a first solution are now registered

--- code/logManager.py
+++ code/logManager.py
@@ -1,33 +1,34 @@
-from time import time
+from datetime import datetime
+# from singletonMeta import SingletonMeta
 
 
-from singletonMeta import SingletonMeta
+class LogManager:
 
-
-class LogManager(metaclass=SingletonMeta):
-
-    def __init__(self):
-        self.__start_time = time()
+    def __init__(self, steps):
+        self.__start_time = datetime.now()
+        self.__counter = 0
+        self.__steps_for_first_solution = steps
         log_text = f'Time: {self.__start_time}'
         f = open('log.txt', 'w')
         f.write(log_text)
         f.close()
 
     def write_on_log(self, operation, num_nodes):
+        self.__counter += 1
         log_text = f"""
 -------------------------------------------------
-Time: {time()}
+Time: {datetime.now()}
 Operation: {operation}
 Number of nodes in the solution: {num_nodes}
 
 """
-        f = open('log.txt', 'w')
+        f = open('log.txt', 'a')
         f.write(log_text)
         f.close()
 
-    def quit(self):
-        log_text = f'Session ended at time: {time()}'
+    def quit(self, status=''):
+        log_text = f'Session ended at time: {datetime.now()}{status}, operations made: {self.__counter - self.__steps_for_first_solution}'
         log_text = "\n" + f'{log_text}' + "\n" + "\n"
-        f = open('log.txt', 'w')
+        f = open('log.txt', 'a')
         f.write(log_text)
         f.close()
