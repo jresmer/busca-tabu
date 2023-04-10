f21c098be0334b717f25739c0c6891b839e9a91f
jresmer <jpresmer@gmail.com>
2023-04-10 17:06:45 -0300

    previous logs are no longer deleted

--- code/logManager.py
+++ code/logManager.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-# from singletonMeta import SingletonMeta
 
 
 class LogManager:
@@ -8,27 +7,29 @@ class LogManager:
         self.__start_time = datetime.now()
         self.__counter = 0
         self.__steps_for_first_solution = steps
+        self.__log_name = f'log-d:{datetime.now()}.txt'
         log_text = f'Time: {self.__start_time}'
-        f = open('log.txt', 'w')
+        f = open(self.__log_name, 'a')
         f.write(log_text)
         f.close()
 
-    def write_on_log(self, operation, num_nodes):
+    def write_on_log(self, operation, num_nodes, solution_value):
         self.__counter += 1
         log_text = f"""
 -------------------------------------------------
 Time: {datetime.now()}
 Operation: {operation}
 Number of nodes in the solution: {num_nodes}
+Solution's value: {solution_value}
 
 """
-        f = open('log.txt', 'a')
+        f = open(self.__log_name, 'a')
         f.write(log_text)
         f.close()
 
     def quit(self, status=''):
         log_text = f'Session ended at time: {datetime.now()}{status}, operations made: {self.__counter - self.__steps_for_first_solution}'
         log_text = "\n" + f'{log_text}' + "\n" + "\n"
-        f = open('log.txt', 'a')
+        f = open(self.__log_name, 'a')
         f.write(log_text)
         f.close()
