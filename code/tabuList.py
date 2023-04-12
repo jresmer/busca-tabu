d34a6935eff94c4c4fb3ea6a167817d943ddc9db
jresmer <jpresmer@gmail.com>
2023-04-11 23:27:18 -0300

    magic mathod__contains__ updated

--- code/tabuList.py
+++ code/tabuList.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
+from networkx import graph_edit_distance
+
+
 class TabuList:
 
     # CRIA LISTA DE SOLUCOES
     # SETTA NUMERO DE ITERACOES QUE UMA SOLUCAO DEVE PERMANECER NA LISTA
-    def __init__(self, iteration_counter=4, *args):
+    def __init__(self, iteration_counter=2, *args):
         self.__iteration_counter = iteration_counter
         self.__iteration_reset_value = iteration_counter
         self.__list = list(args)
 
     # MAGIC METHOD CHECAGEM IN
     def __contains__(self, item):
-        return item in self.__list
+        for _solution in self.__list:
+            if graph_edit_distance(item, _solution) == 0:
+                return True
+        return False
 
     # ADICIONA E REMOVE SOLUCOES TABU
     def update(self, item=None):
