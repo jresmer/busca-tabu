5106d206f5a3ea58eeda8bef983a17b7795d7c4d
jresmer <jpresmer@gmail.com>
2023-05-08 15:25:55 -0300

    first commit

---
+++ code/csvLogManager.py
@@ -0,0 +1,37 @@
+import csv
+import time
+
+
+class CSVLogManager:
+
+
+    def __init__(self, steps, address, dist, budget, perc):
+        city = address.split(", ")
+        city = city[1]
+        self.__starting_time = time.time()
+        self.__filename = f'{city}-log.csv'
+        with open(self.__filename, 'a', newline='') as csv_file:
+            fieldnames = ['Adress', 'Steps', 'Radius', 'Budget', 'Tolerance']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow({'Adress' : str(address), 'Steps' : str(steps),
+                               'Radius' : str(dist), 'Budget' : str(budget), 'Tolerance' : str(perc)})
+        with open(self.__filename, 'a', newline='') as csv_file:
+            fieldnames = ['Operation', 'Number of Nodes', 'Value', 'Time']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+
+    def write_on_log(self, operation, num_nodes, solution_value):
+        with open(self.__filename, 'a', newline='') as csv_file:
+            fieldnames = ['Operation', 'Number of Nodes', 'Value', 'Time']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writerow({'Operation' : operation, 'Number of Nodes' : str(num_nodes),
+                              'Value' : str(solution_value), 'Time' : str(time.time() - self.__starting_time)})
+
+    def quit(self, status=''):
+        with open(self.__filename, 'a', newline='') as csv_file:
+            fieldnames = ['Status', 'Ending Time']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow({'Status' : status, 'Ending Time' : str(time.time() - self.__starting_time)})
+
