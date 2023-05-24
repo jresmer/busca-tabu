458aa7c99068c80410deb3cadcf9d00ffdc74946
jresmer <jpresmer@gmail.com>
2023-05-24 18:03:52 -0300

    aspiration method removed

--- code/tabuSearch.py
+++ code/tabuSearch.py
@@ -13,7 +13,6 @@ class TabuSearch:
 
         self.__best_s = None
         self.__best_s_value = None
-        self.__perc = 100
 
         self.__s = None
         self.__opt_value = None
@@ -21,8 +20,7 @@ class TabuSearch:
 
         self.__address_list = ['Rua Roberto Sampaio Gonzaga, Florianópolis, Brazil', 'São Paulo, Brazil', 'Balneário Camboriú, Brazil', '350 5th Ave, New York, New York']
 
-    def __set_first_solution(self, address_sel, dist, steps, perc, budget):
-        self.__perc = perc
+    def __set_first_solution(self, address_sel, dist, budget):
 
         graph = ox.graph_from_address(self.__address_list[address_sel], network_type='drive', dist=dist)
         _graph = ox.project_graph(graph)
@@ -39,15 +37,13 @@ class TabuSearch:
             self.__opt_value = best_neighbours_value
             budget -= cost
 
-        self.__opt_value_5 = self.__opt_value * (self.__perc / 100)
-
         self.__best_s = self.__s
         self.__best_s_value = self.__opt_value
 
-    def __loop(self, budget):
+    def __loop(self, budget, itr=500):
         max_bool = True
         budget = 0
-        while True:
+        for _ in range(itr):
             # reverte uma alteracao recente:
             max_bool = not max_bool
             self.__s, added_budget = self.__nd.reverse_change(self.__s, max_bool)
@@ -56,17 +52,12 @@ class TabuSearch:
             best_neighbour, \
                 best_neighbours_value, cost = self.__nd.get_best_neighbour_random(self.__s,
                                                                                   budget, max_bool)
-            if best_neighbours_value - self.__opt_value < self.__opt_value_5:
-                self.__opt_value = best_neighbours_value
-                self.__opt_value_5 = best_neighbours_value * (self.__perc / 100)
-                self.__s = best_neighbour
-                budget -= cost
-                if best_neighbours_value < self.__best_s_value:
-                    self.__best_s = best_neighbour
-                    self.__best_s_value = best_neighbours_value
-            else:
-                self.__log.quit(', because no better neighbour was found')
-                break
+            self.__opt_value = best_neighbours_value
+            self.__s = best_neighbour
+            budget -= cost
+            if best_neighbours_value < self.__best_s_value:
+                self.__best_s = best_neighbour
+                self.__best_s_value = best_neighbours_value
 
     def get_best_solution(self):
         return self.__best_s
@@ -79,6 +70,6 @@ class TabuSearch:
             address_sel, dist, steps, budget, perc = self.__interface.address_selection(self.__address_list)
         self.__log = CSVLogManager(steps, self.__address_list[address_sel], dist, budget, perc)
         self.__nd.set_log(self.__log)
-        self.__set_first_solution(address_sel, dist, steps, perc, budget)
+        self.__set_first_solution(address_sel, dist, budget)
         self.__loop(budget)
         self.__log.quit()
