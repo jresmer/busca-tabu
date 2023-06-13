33ab3ac60fa7dfd3a5153fa546afe91ddf84c99d
jresmer <jpresmer@gmail.com>
2023-06-13 14:36:43 -0300

    parallel_random_obj_func implemented

--- code/objFuncCalculator.py
+++ code/objFuncCalculator.py
@@ -1,38 +1,38 @@
 from random import choice
+from random import sample
 import networkx as nx
+import concurrent.futures as cf
 
 
 class ObjFuncCalculator:
 
-    def obj_func_random(self, solution, num_tests=100, attempt_limit=10000) -> float:
-        node_list = list(solution.nodes)
-        self.pair_list = []
+    def __init__(self) -> None:
+        self.__tested_pairs = []
+
+    def random_obj_func(self, solution, num_tests=100, attempt_limit=10000) -> float:
+        edges = [e for e in solution.edges]
+        pair_list = []
 
         t_routes = 0
         t_time = 0
 
         while attempt_limit > 0:
-            length = len(node_list) - 1
+            length = len(edges) - 1
             if length == -1:
                 break
 
-            origin = choice(node_list)
-            dest = choice(node_list)
+            origin, dest, k = choice(edges)
 
-            if origin == dest or (origin, dest) in self.pair_list:
+            if (origin, dest) in pair_list:
                 continue
 
-            self.pair_list.append((origin, dest))
-
-            if not nx.has_path(solution, origin, dest):
-                continue
+            pair_list.append((origin, dest))
 
             shortest_path_length = nx.shortest_path_length(solution, origin, dest,
                                                            weight='travel_time')
-
-            if shortest_path_length != 0:
-                t_time += shortest_path_length
-                t_routes += 1
+            
+            t_time += shortest_path_length
+            t_routes += 1
 
             if t_routes == num_tests:
                 break
@@ -40,3 +40,31 @@ class ObjFuncCalculator:
         if t_routes > 0:
             return t_time / t_routes
         return 0
+    
+        @staticmethod
+        def __time_step(solution, pair):
+            if (pair[0], pair[1]) in self.__tested_pairs: return 0
+
+            self.__tested_pairs.append((pair[0], pair[1]))
+
+            shortest_path_length = nx.shortest_path_length(solution, pair[0],
+                                                           pair[1], weight='travel_time')
+            
+            return shortest_path_length
+        
+        def parallel_random_obj_func(self, solution, num_tests=None):
+            with cf.ProcessPoolExecutor() as pool_executor:
+                edges = [e for e in solution.edges]
+                if num_tests is None: num_tests = len(edges)
+                edges_ = sample(edges, num_tests)
+                futures = {pool_executor.submit(self.__time_step, edge) for edge in edges}
+
+                total_time = 0
+                for future in cf.as_completed(futures):
+                    total_time += future.result()
+
+                pool_executor.shutdown()
+
+            self.__tested_pairs = []
+
+            return total_time / num_tests
