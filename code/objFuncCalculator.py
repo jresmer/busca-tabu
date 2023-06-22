a2e963219cbe918562ccc6cc0796c7667e64c29b
jresmer <jpresmer@gmail.com>
2023-06-22 20:15:41 -0300

    fixed

--- code/objFuncCalculator.py
+++ code/objFuncCalculator.py
@@ -1,7 +1,5 @@
 from random import choice
-from random import sample
 import networkx as nx
-import concurrent.futures as cf
 
 
 class ObjFuncCalculator:
@@ -40,30 +38,3 @@ class ObjFuncCalculator:
         if t_routes > 0:
             return t_time / t_routes
         return 0
-    
-        @staticmethod
-        def __time_step(solution, pair):
-            if (pair[0], pair[1]) in self.__tested_pairs: return 0
-
-            self.__tested_pairs.append((pair[0], pair[1]))
-
-            shortest_path_length = nx.shortest_path_length(solution, pair[0],
-                                                           pair[1], weight='travel_time')
-            
-            return shortest_path_length
-        
-        def parallel_random_obj_func(self, solution, num_tests):
-            with cf.ProcessPoolExecutor() as pool_executor:
-                edges = [e for e in solution.edges]
-                edges_ = sample(edges, num_tests)
-                futures = {pool_executor.submit(self.__time_step, edge) for edge in edges_}
-
-                total_time = 0
-                for future in cf.as_completed(futures):
-                    total_time += future.result()
-
-                pool_executor.shutdown()
-
-            self.__tested_pairs = []
-
-            return total_time / num_tests
