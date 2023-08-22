b135ed24ce78c008a1e23c9a7ea7404bffdfdde3
jresmer <jpresmer@gmail.com>
2023-08-22 16:04:58 -0300

    new test set

--- code/tester.py
+++ code/tester.py
@@ -7,10 +7,10 @@ class Tester:
     @staticmethod
     def run_test():
         dao = SolutionDAO()
-        for dist in [500, 1500]:
-            for budget in [30000, 80000, 130000]:
-                for tabu_list_size in [20, 40, 80]:
-                    for itr in [50, 75]:
+        for dist in [500, 1000]:
+            for budget in [80000, 130000]:
+                for tabu_list_size in [20, 40]:
+                    for itr in [25, 50, 75]:
                         tabu_search = TabuSearch()
                         tabu_search.run(1, dist, budget, tabu_list_size, itr=itr)
                         dao.add(tabu_search.get_best_solution())
